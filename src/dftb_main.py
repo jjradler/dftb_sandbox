368b549a984275081565bcdaf42e469832976ece
jjradler <jjradler@uw.edu>
2018-02-16 13:45:28 -0500

    made some changes to the structure of the modules

---
+++ src/dftb_main.py
@@ -0,0 +1,13 @@
+#dftb_main.py
+#Script for calling the various modules of DFTB calculation
+#Written By:    J. J. Radler
+#Date Written:  02/15/2018
+#Date Appended: 02/15/2018
+#-----------------------------------------------------------
+
+import numpy as np
+import scipy as sp
+import sys
+# Parse Input Files
+
+
