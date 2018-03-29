30b5dc9e6f8ee6e2483569eaa2f00ea0bff29b13
Joseph J. Radler <jjradler@uw.edu>
2018-03-28 17:10:12 -0700

    added a substantial amount of code and debugged matrixbuilder and molecule files. Still have a ways to go before this sucker runs, though...

--- dftb_sandbox/dftb_sandbox/matrixbuilder.py
+++ dftb_sandbox/dftb_sandbox/matrixbuilder.py
@@ -1,4 +1,4 @@
-# matbuild.py
+# matrixbuilder.py
 """matbuild.py constructs the intial Molecule class matrix members
 and also contains functions for updating the Fock matrix during SCC
 iterations based on the Molecule and AtomPair class object member
@@ -7,85 +7,91 @@ parameters."""
 __author__ = "Joseph J. Radler"
 
 import numpy as np
-from numpy import linalg as lg
-from numpy import dot
-#import scipy as sp
-#from .molecule import *
+#from numpy import linalg as lg
+#from numpy import dot
+#from .atompair_properties import AtomPair
+#from .atom_properties import Atom
+#from .molecule_properties import Molecule
+#from .helpers import *
 
-#def buildfock0_i(self, atoms_list):
-#    """builds the zeroth-order Fock matrix for iteration 1 assuming
-#    l_max = 0 (sigma bonds only for now, with H_2)"""
-#    M = len(atoms_list)
-#    fock0_i = np.empty((M, M), dtype=float)
-#    idx = 0
-#    jdx = 0
-#
-#    for idx in enumerate(atoms_list):
-#        for jdx in enumerate(atoms_list):
-#            if idx is jdx:
-#                fock0_i[idx,jdx] = atoms_list[idx].e_ks
-#            elif idx < jdx:
-#                fock0_i[idx,jdx] = atoms_list[idx].hss
-#                fock0_i[jdx,idx] = atoms_list[idx].hss
-#            else:
-#                fock0_i[jdx,idx] = 0.00
-#
-#    return fock0_i
-#
-#def fock1_update(self, fock1_i):
-#    """updates the Fock^1 matrix from updated dQ_list and Molecule params."""
-#    pass
-#
-#    return fock1_k
-#
-#def buildfock1_i(self, atoms_list):
-#    """builds initial Fock^1 matrix from dqGuess_list and Molecule params."""
-#    dq = 0
-#    idx = 0
-#    jdx = 0
-#    M = len(atoms_list)
-#    fock1_i = np.empty((M, M), dtype=float)
-#    for idx in enumerate(atoms_list):
-#        for jdx in enumerate(atoms_list):
-#            if idx is jdx:
-#                fock1_i = 0.0
-#            elif idx < jdx:
-#                fock1_i[idx, jdx] = atompairs_list[idx].fock1_ab
-#                fock1_i[jdx, idx] = fock1_i[idx, jdx]
-#
-#    return fock1_i
-#
-#def buildoverlap(self, atoms_list):
-#    M = len(atoms_list)
-#    idx = 0
-#    jdx = 0
-#    overlap = np.empty((M, M), dtype=float)
-#
-#    for idx in enumerate(atoms_list):
-#        for jdx in enumerate(atoms_list):
-#            if idx is jdx:
-#                overlap[idx,jdx] = 1.0
-#            elif idx < jdx:
-#                overlap[idx, jdx] = atompairs_list[idx].overlap_ab
-#                overlap[jdx, idx] = overlap[idx, jdx]
-#
-#    return overlap
-#
-#def buildtotalfock_i(self, fock0_i, fock1_i, overlap):
-#    """builds the total Fock matrix from fock0 and fock1"""
-#    totalfock_i = fock0_i + fock1_i.dot(overlap)
-#
-#    return totalfock_i
+def buildfock0_i(self, atoms_list):
+    """builds the zeroth-order Fock matrix for iteration 1 assuming
+    l_max = 0 (sigma bonds only for now, with H_2)"""
+    # TODO: fix the calls and the way the matrices are constructed between this\
+    #        and molecule properties
+    m_atoms = len(atoms_list)
+    fock0_i = np.empty((m_atoms, m_atoms), dtype=float)
+    idx = 0
+    jdx = 0
+
+    for idx in enumerate(atoms_list):
+        for jdx in enumerate(atoms_list):
+            if idx is jdx:
+                fock0_i[idx,jdx] = atoms_list[idx].e_ks
+            elif idx < jdx:
+                fock0_i[idx,jdx] = atoms_list[idx].hss
+                fock0_i[jdx,idx] = atoms_list[idx].hss
+            else:
+                fock0_i[jdx,idx] = 0.00
+
+    return fock0_i
+
+def fock1_update(self, fock1_i):
+    # TODO: add in the update expressions for H[mu, nu]
+    """updates the Fock^1 matrix from updated dQ_list and Molecule params."""
+    pass
 
 
-#def build_c_i(self, atoms_list):
-    #"""docstring"""
-    #pass
-    # return cvec_i
+def buildfock1_i(self, atoms_list, atompairs_list):
+    """builds initial Fock^1 matrix from dqGuess_list and Molecule params."""
+    # TODO: write this in terms of the matrix blocks from AtomPairs
+    dq = 0
+    idx = 0
+    jdx = 0
+    M = len(atoms_list)
+    fock1_i = np.empty((M, M), dtype=float)
+    for idx in enumerate(atoms_list):
+        for jdx in enumerate(atoms_list):
+            if idx is jdx:
+                fock1_i = 0.0
+            elif idx < jdx:
+                fock1_i[idx, jdx] = atompairs_list[idx].fock1_ab
+                fock1_i[jdx, idx] = fock1_i[idx, jdx]
+
+    return fock1_i
+
+def buildoverlap(self, atoms_list, atompairs_list):
+    # TODO: Write this in terms of the AtomPair overlap blocks
+    M = len(atoms_list)
+    idx = 0
+    jdx = 0
+    overlap = np.empty((M, M), dtype=float)
+
+    for idx in enumerate(atoms_list):
+        for jdx in enumerate(atoms_list):
+            if idx is jdx:
+                overlap[idx,jdx] = 1.0
+            elif idx < jdx:
+                overlap[idx, jdx] = atompairs_list[idx].overlap_ab
+                overlap[jdx, idx] = overlap[idx, jdx]
+
+    return overlap
+
+def buildtotalfock_i(self, fock0_i, fock1_i, overlap):
+    """builds the total Fock matrix from fock0 and fock1"""
+    totalfock_i = fock0_i + fock1_i.dot(overlap)
+
+    return totalfock_i
 
 
-#def dQ_rebuild(self, c_vec, overlap):
-#    """docstring"""
-#    pass
+def build_cvec(self, atoms_list):
+   """docstring"""
+   # TODO: add a docstring and figure out expressions to generate c_vec from dQ
+   pass
 
-#    return dQ_k
+
+def dQ_rebuild(self, c_vec, overlap):
+    """docstring"""
+    # TODO: how do I extract the updated dQ from the c_vector?
+    pass
+
--- dftb_sandbox/dftb_sandbox/molecule_properties.py
+++ dftb_sandbox/dftb_sandbox/molecule_properties.py
@@ -1,4 +1,4 @@
-#moleculeproperties.py
+#molecule_properties.py
 """
     atomSpec.py defines classes for:
         Atom objects that contain calculated molecular total energy
@@ -13,15 +13,17 @@
 __author__ = "Joseph J. Radler"
 
 import numpy as np
-from numpy import linalg as lg
-from .singlepoint import SinglePoint
-from .atomproperties import Atom
-from .atompairproperties import AtomPair
-from matrixbuilder import fock0
-from matrixbuilder import fock1
-from matrixbuilder import fock_tot
-from matrixbuilder import C_vec
-from helpers import *
+#from numpy import linalg as lg
+from numpy import size as size
+from numpy import sqrt as sqrt
+#from matrixbuilder import buildfock0_i
+#from matrixbuilder import buildfock1_i
+#from matrixbuilder import buildfocktotal_i
+#from matrixbuilder import build_cvec
+#from .singlepoint import SinglePoint
+#from .atom_properties import Atom
+#from .atompair_properties import AtomPair
+#from helpers import *
 
 class Molecule(object):
     """Molecule objects contain information about the Atom objects and
@@ -41,28 +43,51 @@ class Molecule(object):
         fock1_i
         overlap
     """
-    self.atoms_list = atoms_list
-    self.atompairs_list = atompairs_list
 
     def __init__(self, name, atoms_list, atompairs_list):
         """Instantiate members of Molecule object"""
+        self.atoms_list = atoms_list
+        self.atompairs_list = atompairs_list
         self.name = name    # Generate a formula ``name`` from atom list
-        self.ntot_elec = None   # Compute from sum over all AtomPairs
-        self.M = len(atoms_list)
-        #self.ptot_pairs = None
-        self.c_vec = None
-        self.Ftot_i = None
-        self.F0_i = None
-        self.F1_i = None
-        self.S = np.empty((2 * M, 2 * M), dtype=float)
+        self._m_atoms = None
+        self._n_electrons = None
+        self._p_pairs = None
+        self.__rowcol_size = np.int(sqrt(size(atoms_list[0].sab_block)))
+        self.overlap = np.zeros((self.__rowcol_size, self.__rowcol_size))
+        self.fock0_i = np.zeros((self.__rowcol_size, self.__rowcol_size))
+        self.fock1_i = np.zeros((self.__rowcol_size, self.__rowcol_size))
+        self.focktot_i = np.zeros((self.__rowcol_size, self.__rowcol_size))
+        self.c_vec = np.ones(self.__rowcol_size)
 
-        # Member instantiation statements
+    def num_atoms(self):
+        """computes the number of atoms in the molecule class object"""
+        self._m_atoms = len(self.atoms_list)
 
-        self.ntot_elec = set_ntot_elec(self, atoms_list)
-        self.mtot_atoms = len(atoms_list)
-        self.S = buildmatrix.overlap(atoms_list)
-        self.F0_i = fock0(atompairs_list)
-        self.F1_i = fock1(atompairs_list)
-        self.Ftot_i = fock_tot(self.F0_i, self.F1_i,\
-                self.S)
-        self.c_vec = cvec_i(atoms_list)
+    def num_electrons(self):
+        """sums the number of total electrons in the molecule valence."""
+        self._n_electrons = 0        #default
+        idx = 0                     # initialize loop index
+
+        for idx in enumerate(self.atoms_list):
+            self._n_electrons += self.atoms_list[idx].n_elec
+
+    def num_pairs(self):
+        """sums the number of atom pairs in the molecule from atom_pairlist"""
+        self._p_pairs = 0
+        idx = 0
+
+        for idx in enumerate(self.atompairs_list):
+            self._p_pairs += self.atompairs_list[idx].n_elec
+
+    # Member instantiation statements
+    #num_atoms(self)
+    #num_electrons(self)
+    #num_pairs(self)
+    #self.ntot_elec = set_ntot_elec(self.atoms_list)
+    #self.mtot_atoms = len(self.atoms_list)
+    #self.S = buildoverlap(self.atoms_list)
+    #self.F0_i = buildfock0_i(self.atompairs_list)
+    #self.F1_i = buildfock1_i(self.atompairs_list)
+    #self.Ftot_i = buildtotalfock_i(self.F0_i, self.F1_i,\
+    #        self.S)
+    #self.c_vec = cvec_i(self.atoms_list)
